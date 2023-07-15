f24a050a78d1bcdbf0378de22ed704ac0dedc069
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-07-15 14:53:03 +0300

    Add scriptable object.

---
+++ Assets/_Scripts/LegoType.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace _Scripts
+{
+    public enum LegoType
+    {
+        Cube,
+        Sphere,
+        Cylinder
+    }
+}
---
+++ Assets/_Scripts/Scriptable Object/LegoSO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scripts
+{
+    [CreateAssetMenu]
+    public class LegoSO : ScriptableObject
+    {
+        public MyLego legoA;
+        
+        
+        [Serializable]
+            public struct MyLego
+            {
+                public Transform legoPrefab;
+                public LegoType type;
+            }
+    }
+}
