b62d1d5fc5c89cd6f412538d9b9b28b1c1203390
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-07-19 17:54:03 +0300

    Update Airplane.cs

--- Assets/_Scripts/Airplane.cs
+++ Assets/_Scripts/Airplane.cs
@@ -4,16 +4,6 @@ namespace _Scripts
 {
     public class Airplane : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
         
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
